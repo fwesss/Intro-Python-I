89bbda7c8855a6261c3f193a7cefc0063ab6e54b
Westley Feller <feller.westley@gmail.com>
2019-11-26 22:02:29 -0800

    Using Sieve of Eratosthenes

--- src/stretch_prime.py
+++ src/stretch_prime.py
@@ -1,10 +1,30 @@
 def check_prime(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return False
+    prime = True
+    p = 2
+    number_set = [x for x in range(2, number + 1)]
 
-    return True
+    def sift_number_set(start, updated_number_set):
+        nonlocal prime
+        for i in range(start, number + 1, start)[1:]:
+            if i in updated_number_set:
+                if i == number:
+                    prime = False
+                else:
+                    updated_number_set.remove(i)
+
+        return updated_number_set
+
+    def increment_p(updated_p):
+        for val in number_set:
+            if val > updated_p:
+                return val
+
+    while p < number_set[-1] and prime is True:
+        sift_number_set(p, number_set)
+        p = increment_p(p)
+
+    return prime
 
 
 if __name__ == '__main__':
-    print(check_prime(18))
+    print(check_prime(2))
