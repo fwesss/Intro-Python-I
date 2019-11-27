9c072854e2b7e4b18f49634e23cb3ff4bfdab1cd
Westley Feller <feller.westley@gmail.com>
2019-11-26 18:29:52 -0800

    Step 11 completed

--- src/10_functions.py
+++ src/10_functions.py
@@ -6,7 +6,16 @@
 num = input("Enter a number: ")
 num = int(num)
 
+
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def is_even(number):
+    if number % 2 == 0:
+        return True
 
+
+if is_even(num):
+    print("Even!")
+else:
+    print("Odd")
